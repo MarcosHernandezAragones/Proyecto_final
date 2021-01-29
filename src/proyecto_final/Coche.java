52107111118750a474dfdc2a4191ccdfc0198f36
Marcos Hernández Aragonés <marcoshernandezaragones@gmail.com>
2021-01-29 10:43:40 +0100

    actualizado

---
+++ src/proyecto_final/Coche.java
@@ -0,0 +1,116 @@
+package proyecto_final;
+
+public class Coche {
+	
+	private String nombrePiloto;
+	private int dorsal;
+	private int distanciaCarrera;
+	private String estadoCoche;
+	private final int POTENCIA=50;
+	private int velocidad;
+	private int kmRecorridos;
+
+
+	public Coche(String nombrePiloto, int dorsal) {
+		super();
+		this.nombrePiloto = nombrePiloto;
+		this.dorsal = dorsal;
+		this.estadoCoche="PARADO";
+		this.velocidad=0;
+		this.kmRecorridos=0;
+		distanciaCarrera = 300; //De momento
+	}
+
+
+	public String getNombrePiloto() {
+		return nombrePiloto;
+	}
+	public void setNombrePiloto(String nombrePiloto) {
+		this.nombrePiloto = nombrePiloto;
+	}
+	public int getDorsal() {
+		return dorsal;
+	}
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+	public String getEstadoCoche() {
+		return estadoCoche;
+	}
+	public void setEstadoCoche(String estadoCoche) {
+		this.estadoCoche = estadoCoche;
+	}
+	public int getVelocidad() {
+		return velocidad;
+	}
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+	public int getKmRecorridos() {
+		return kmRecorridos;
+	}
+	public void setKmRecorridos(int kmRecorridos) {
+		this.kmRecorridos = kmRecorridos;
+	}
+	public int getDistanciaCarrera() {
+		return distanciaCarrera;
+	}
+	public int getPotencia() {
+		return POTENCIA;
+	}
+	
+	public void arrancar() {
+		
+		
+		switch (this.estadoCoche) {
+		case "PARADO":
+			this.setEstadoCoche("MARCHA");
+			System.out.println("Has arrancado el coche");
+			break;
+		case "MARCHA":
+			System.out.println("El coche ya esta arrancado, has perdido un turno");
+			break;
+			
+		case "ACCIDENTADO":
+			System.out.println("Tu coche esta accidentado, debes rearrancarlo en el turno siguiente");
+			break;
+			
+		case "TERMINADO":
+			System.out.println("Ya has terminado la carrera");
+			break;
+		
+		}
+		
+	}
+
+	public void acelerar() {
+		
+		switch (this.getEstadoCoche()) {
+		
+		case "PARADO":
+			System.out.println("Para acelerar, primero debes arrancar el coche en el turno siguiente");
+			break;
+		
+		case "MARCHA":
+			double random = Math.random()*this.POTENCIA+1;
+			int acel = (int)Math.floor(random);
+			velocidad+=acel;
+			if (velocidad>200) {
+				estadoCoche = "ACCIDENTADO";
+				velocidad=0;
+			}else {
+				kmRecorridos+=acel;
+			}
+			break;
+			
+		case "ACCIDENTADO":
+			System.out.println("Para poder acelerar, debes rearrancar el coche en el turno siguiente");
+			break;
+			
+		case "TERMINADO":
+			System.out.println("Ya has terminado la carrera");
+			break;
+	
+}
+	}
+}
---
+++ src/proyecto_final/main.java
@@ -0,0 +1,51 @@
+package proyecto_final;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static int pintaMenu() {
+		int opc =0;
+		Scanner leer = new Scanner(System.in);
+		
+		do {
+			System.out.println("Pulsa 1 para arrancar");
+			System.out.println("Pulsa 2 para acelerar");
+			System.out.println("Pulsa 3 para frenar");
+			System.out.println("Pulsa 4 para rearrancar");
+			opc = leer.nextInt();
+		} while (opc<1 || opc>4);
+		return opc;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Coche car = new Coche("Yo", 23);
+		int opc=0, km=car.getKmRecorridos();
+		
+		
+		do {
+			opc=pintaMenu();
+			switch (opc) {
+			case 1:
+				car.arrancar();
+				System.out.println();
+				break;
+			case 2:
+				car.acelerar();
+				System.out.println();
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			}
+			
+		} while (car.getKmRecorridos()<=car.getDistanciaCarrera());
+	}
+
+}
+	
